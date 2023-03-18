b883173a47289a7fb2d7125c5076274b89930eff
Juan Games <119117961+JuanGames45@users.noreply.github.com>
2023-03-19 00:32:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -79,8 +79,7 @@ class MyFooter extends HTMLElement {
 
             <!--Webcounter y rss aquí-->
             <div id="td2"><!-- hitwebcounter Code START -->
-<a href="https://www.hitwebcounter.com" target="_blank">
-<img src="https://hitwebcounter.com/counter/counter.php?page=8386886&style=0005&nbdigits=5&type=ip&initCount=0" title="Free Counter" Alt="web counter"   border="0" /></a>  
+
             </div>    
             <br>
             <br>
