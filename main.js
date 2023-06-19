70fcbcf1ae9e7bc2bbf8645fa16b7ab4182a40be
Juan Games <119117961+JuanGames45@users.noreply.github.com>
2023-06-20 00:27:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,266 +1,114 @@
-/*Poner background color solo en ID content a menos que sea necesario*/
-/*Hay que poner id back para el fondo blanco de toda la pagina, así será todo por bloques*/
-* {
-  box-sizing: border-box;
+//Modificar aquí modificará todo el sitio web
+var links = document.querySelectorAll(".link");
+for(link of links){
+  link.addEventListener("click", function(e){
+    for(inlink of links){
+      inlink.classList.remove("active");
+    }
+    e.target.classList.add("active");
+  });
 }
+//Plantilla cabecera
+class MyHeader extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+        <head>
+            <link rel="icon" type="image/x-con" href="https://www.juangames.com/media/images/favicon.png">
+        </head>
+        <header>
 
-body{
+            <title>Juan Games Blog</title>
+            <img src="https://www.juangames.com/media/images/construction.webp" class="banner">
+            <img src="https://www.juangames.com/media/images/backgrounds/banner.png" height="672" width="1920" class="banner">
+            
+                
+            <div class="menu">
+                <ul class="menu-buttons">
+                    <li class="imglink"><a href="https://www.juangames.com/index.html"><img src="https://www.juangames.com/media/images/buttons/home-button.png" width="90px" height="60px"></a></li>
+                    <a href="https://www.juangames.com/blog.html"><li class="imglink"><img src="https://www.juangames.com/media/images/buttons/blog-button.png"></li></a>
+                    <a href="https://www.juangames.com/art.html"><li class="imglink"><img src="https://www.juangames.com/media/images/buttons/art-button.png"></li></a>
+                    <li class="imglink"><a href="https://www.juangames.com/about.html"><img src="https://www.juangames.com/media/images/buttons/about-button.png"></a></li>
 
-  background: url(https://www.juangames.com/media/images/backgrounds/background.png);
-  background-repeat: repeat;
-  padding: 20px 40px 20px 40px;
+                </ul>
+    
+            </div>           
+                
+            </header>`
+        
+    }
 }
+customElements.define('my-header', MyHeader)
 
-@font-face {
-  font-family: 'MS Sans win 95';
-  src: url('https://www.juangames.com/media/fonts/Ms-Sans-Serif-8pt.ttf');
+//Plantilla barra lateral
+class MySidebar extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+        <div id="sidebar">
+            
+        <div>
+            <h3 id="recent">My social media:</h3>
+            <br>
+            <a href="https://juangames45.newgrounds.com" target="_blank"><img src="https://www.juangames.com/media/images/buttons/ng-button.gif" width="176" heigh="62" class="imglink" id="sidebutton"></a>
+            <br>
+            <a href="https://twitter.com/JuanGames45" target="_blank"><img src="https://www.juangames.com/media/images/buttons/twitter-button.gif" width="176" heigh="62" class="imglink" id="sidebutton"></a>
+            <br>
+            <a href="" target="_blank"><img src="https://www.juangames.com/media/images/buttons/button-background.png" width="176" heigh="62" class="imglink" id="sidebutton"></a>
+        </div>
+        <p id="img_comment">(This is just a placeholder, only the NG button works properly)</p>
+        </div id="sidebar">`
+        
+    }
 }
+customElements.define('my-sidebar', MySidebar)
 
-@font-face {
-  font-family: 'Oswald';
-  src: url(https://www.juangames.com/media/fonts/Oswald-Regular.ttf);
+//Plantilla Footer
+class MyFooter extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+        <footer>
+          
+        <div id="le-footer">
+            <!--Botones aquí -->
+            <div id="td1">
+                <img src="https://www.juangames.com/media/images/buttons/notepad2.gif" width="120" height="40" id="footer">
+                <img src="https://www.juangames.com/media/images/buttons/any_browser.gif" width="120" height="40">
+                <img src="https://www.juangames.com/media/images/buttons/besteyes.gif" width="120" height="40">
+                <br>
+                <img src="https://www.juangames.com/media/images/buttons/bl64.gif" width="119" height="40">
+                <a href="mailto:contact@juangames.com"><img src="https://www.juangames.com/media/images/buttons/email-icon.gif" width="119" height="40" class="imglink"></a>
+                <img src="https://www.juangames.com/media/images/buttons/blinchik.gif" width="119" height="40">
+
+            </div>
+
+            <!--Webcounter y rss aquí-->
+            <div id="td2">
+            <!-- hitwebcounter Code START -->
+            <a href="https://www.hitwebcounter.com" target="_blank">
+            <img src="https://hitwebcounter.com/counter/counter.php?page=8386870&style=0001&nbdigits=5&type=ip&initCount=0" title="Free Counter" Alt="web counter"   border="0" /></a>
+            </div>    
+            <br>
+            <br>
+            <br>
+            <div id="td3">
+            <br><br><br><br><br><br>
+            <p id="td3">© 2022-2023 Juan Games</p>
+            <a href="https://pages.github.com/" target="_blank"><img src="https://www.juangames.com/media/images/buttons/powered-by-github.png" width="119" height="40" class="imglink"></a>
+            
+            </div>
+        </div id="le-footer">
+
+         </footer>`
+        
+    }
 }
+customElements.define('my-footer', MyFooter)
 
-/*Cositas del Menú*/
-.menu{
-  margin-top: 40px;
-  width: 100%;
-  margin-bottom: 50px;
+class ReadMore extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+    
+        <button class="button">Read More>></button>`
+        
+    }
 }
-
-.menu .menu-buttons{
- 
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  list-style-type: none;
-  height: 60px;
-  
-}
-.menu li{
-  
-  color: black;
-  text-align: center;
-  padding: 10px;
-  width: 90px;
-  height: 60px;
-  margin: 0 5px 0 0;
-  font-family: 'Oswald', sans-serif;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.menu a{
-  text-decoration: none;
-}
-
-
-
-.imglink:hover{
-  -webkit-filter: brightness(70%);
-}
-
-.imglink:active{
-  -webkit-filter: brightness(55%);
-}
-
-/*Contenedor de la pagina*/
-/*Estandar de los años 2000 es 700px, alternativa 1070px o 900px*/
-.back{
-  margin: 15px auto;
-  width: 1070px;
-  background-color: white;
-}
-
-#content{
-  /*Fondo del contenido ↓*/
-  width: 70%;
-  float: left;
-  border-right: black dotted 5px;
-  border-spacing: 20px;
-  margin-left: 20px;
-  padding-bottom: 20px;
-}
-
-#rightcontent{
-  padding-left: 20px;
-  
-  
-  
-}
-
-#rightcontentainer{
-  border-left: black dotted 5px;
-  float: right;
-  width: 70%;
-  border-spacing: 20px;
-  margin-right: 20px;
-  padding-bottom: 20px;
-}
-
-header .banner{
-  height: 100%;
-  width: 100%;
-  align-items: center;
-  min-height: 80px;
-  
-}
-
-#sidebar{
-  width: 25%;
-  height: 100%;
-  margin-right: 20px;
-  background-color: white;
-  float: right;
-  
-}
-
-#leftsidebar{
-  width: 25%;
-  height: 100%;
-  margin-left: 20px;
-  background-color: white;
-  float: left;
-  min-height: 100%;
-}
-
-#sidebutton{
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-
-footer{
-  clear: both;
-  width: 100%;
-  padding-top: 40px;
-  padding-bottom: 10px;
-  
-}
-
-#le-footer{
-  border-top: black dotted 4px;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-#td1{
-  float:left;
-
-  padding-top: 40px;
-}
-#td2{
-  float: right;
-  padding-top: 40px;
-  text-align: left;
-  
-}
-#td3{
-  width: 100%;
-  text-align: center;
-}
-
-/*Textos*/
-p{
-  font-size: 21px;
-  color: black;
-  font-family: 'Ms Sans win 95',;
-  margin-right: 20px;
-  
-}
-
-h1{
-  font-size: 59px;
-  color: black;
-  font-family: 'Times New Roman', sans-serif;
-}
-
-h2{
-  font-size: 40px;
-  color: black;
-  font-family: 'Times New Roman', sans-serif;
-  margin-bottom: 0px;
-}
-
-h3{
-  font-size: 35px;
-  color: black;
-  font-family: 'Times New Roman', sans-serif;
-}
-
-h4{
-  font-size: 30px;
-  color: black;
-  font-family: 'Times New Roman', sans-serif;
-}
-
-a{
-  color: #ff9100;
-  text-decoration: none;
-  
-}
-
-a:hover{
-  text-decoration:underline;
-  color:#ffa835
-}
-
-a:active{
-  color:#1d3d97;
-}
-
-a:visited{
-  
-}
-
-/*Botones*/
-.button{
-  font-size: 21px;
-  font-family: 'Oswald', 'sans-sherif';
-  border-left: none;
-  border-top: none;
-  background: url(https://www.juangames.com/media/images/buttons/button-background.png) repeat;  
-}
-
-.button a{
-  color: black;
-}
-
-.button:hover{
-  -webkit-filter: brightness(85%);
-}
-
-
-
-time{
-  font-size: 27px;
-  font-family: 'Times New Roman', sans-serif;
-  font-style: bold;
-  margin-top: 0px;
-  
-}
-
-#img_comment{
-  text-align: center;
-  font-size: 16px;
-  color: #5a6267;
-}
-
-marquee{
-  font-size: 25px;
-  color: black;
-  font-family: 'Ms Sans win 95';
-  margin-right: 40px;
-}
-
-#recent{
-  margin-bottom: 0px;
-  margin-top: 30px;
-}
-
-.post-title{
-  color: black;
-}
-
-
+customElements.define('read-more', ReadMore)
