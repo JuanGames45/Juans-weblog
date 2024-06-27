3a8ab00eb0b68620461d4c0f3c42b52bb8ef5f7b
Juan Games <119117961+JuanGames45@users.noreply.github.com>
2024-06-28 01:14:45 +0200

    Add files via upload

--- Blogtest/Style/Main.js
+++ Blogtest/Style/Main.js
@@ -1,17 +1,124 @@
 
-const toggleSwitch = document.querySelector
-('.theme-switch input[type="checkbox"]');
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute
-        ('data-theme', 'dark');
+var links = document.querySelectorAll(".link");
+for(link of links){
+  link.addEventListener("click", function(e){
+    for(inlink of links){
+      inlink.classList.remove("active");
     }
-    else {
-        document.documentElement.setAttribute(
-            'data-theme', 'light');
-    }    
+    e.target.classList.add("active");
+  });
 }
+//Plantilla cabecera
+class MyHeader extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+        <head>
+    <link rel="stylesheet" type="text/css" href="Style/Main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Juan Games Blog</title>
+    <link rel="icon" type="image/x-con" href="https://www.juangames.com/media/images/favicon.png">
 
-toggleSwitch.addEventListener('change', switchTheme, false);
-/////////////////////////////////////////////////////////
+    </head>
+
+        <header>
+            <img src="https://www.juangames.com/media/images/construction.webp" class="banner">
+            <img src="https://www.juangames.com/media/images/backgrounds/banner.png" class="banner">
+            <div class="desktopmenu">
+                
+                <button class="menuitem" id="homebutton">Home</button>
+                <button class="menuitem">Blog</button>
+                <button class="menuitem">Art</button>
+                <div class="dropdown">
+                <a href="about.html"><button class="dropbtn" id="about"></button></a>
+                <div class="dropdown-content">
+                    <a class="dropitm">The site</a>
+                    <a class="dropitm">Myself</a>
+                    <a class="dropitm" id="comm">Commissions</a>
+                </div>
+                </div>
+            
+            </div>
+            <div class="mobilemenu">
+                
+                <button class="mbmenuitem" id="homebutton">Home</button>
+                <button class="mbmenuitem">Blog</button>
+                <button class="mbmenuitem">Art</button>
+                <button class="mbmenuitem" id="About">About</button>
+                
+            </div>
+        </header>`
+        
+    }
+}
+customElements.define('my-header', MyHeader)
+
+//Plantilla barra lateral
+class MySidebar extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+        <div id="sidebar">
+        <div class="sidebar">
+
+            <p>Go ahead and leave me.
+                I think I prefer to stay inside.
+                Maybe you'll find someone else
+                To help you.
+                Maybe Black Mesa...
+                THAT WAS A JOKE, HA HA, FAT CHANCE.
+                
+                Anyway this cake is great
+                It's so delicious and moist
+                
+                Look at me still talking when there's science to do
+                When I look out there
+                It makes me GLaD I'm not you.
+                
+                I've experiments to run
+                There is research to be done
+                On the people who are
+                Still alive.
+                
+                And believe me I am still alive
+                I'm doing science and I'm still alive
+                I feel FANTASTIC and I'm still alive
+                While you're dying I'll be still alive
+                And when you're dead I will be still alive
+                Still alive
+                Still alive. </p>
+
+            
+
+        </div>`
+        
+    }
+}
+customElements.define('my-sidebar', MySidebar)
+
+//Plantilla Footer
+class MyFooter extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+        <div class="footerwrapper">
+        <footer>
+            
+
+            <p>🄯 Juan Games 2024.</p>
+            
+
+        </footer>
+        </div>`
+        
+    }
+}
+customElements.define('my-footer', MyFooter)
+
+class ReadMore extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+    
+        <button class="button">Read More>></button>`
+        
+    }
+}
+customElements.define('read-more', ReadMore)
