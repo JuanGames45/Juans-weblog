c58edb4ae8f6ac4129874c8c1ca8eda1955a5505
Juan Games <119117961+JuanGames45@users.noreply.github.com>
2024-07-23 17:11:43 +0200

    Add files via upload

--- style/Main.js
+++ style/Main.js
@@ -16,6 +16,7 @@ class MyHeader extends HTMLElement {
         
     <head>
     <link rel="stylesheet" type="text/css" href="https://www.juangames.com/style/Main.css">
+    <link rel="alternate" type="application/rss+xml" title="JG blog RSS" href="https://www.juangames.com/rss.xml">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Juan Games Blog</title>
     <link rel="icon" type="image/x-con" href="https://www.juangames.com/media/images/favicon.png">
